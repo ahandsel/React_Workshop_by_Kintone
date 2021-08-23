8c35cf3e835ec05e56759e49f9c598c57bfbb5e2
ahandsel <genji-fujimori@cybozu.co.jp>
2021-08-23 16:52:44 +0900

    Remove the POST in server.js

--- backend/server.js
+++ backend/server.js
@@ -48,33 +48,6 @@ app.get('/getData', cors(corsOptions), async (req, res) => {
 // Add the POST request call in the section below
 // - - - - - - - - - - - - - - - - - -
 
-// This runs if a POST request calls for localhost:5000/postData
-
-app.post('/postData', cors(corsOptions), async (req, res) => {
-  const requestBody = {
-    'app': appID,
-    'record': {
-      'title': {
-        'value': req.body.title
-      },
-      'author': {
-        'value': req.body.author
-      }
-    }
-  };
-  const options = {
-    method: 'POST',
-    headers: {
-      'X-Cybozu-API-Token': apiToken,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestBody)
-  }
-  const response = await fetch(singleRecordEndpoint, options);
-  const jsonResponse = await response.json();
-  res.json(jsonResponse);
-});
-
 // - - - - - - - - - - - - - - - - - -
 // Add a New Route for a POST request using singleRecordEndpoint
 
